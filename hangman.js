4abf8fcd8e1836cb6c6ffdc3048d76ca395ab1e1
AhmedRamadan <ahmedramadanmohamedoweis@gmail.com>
2025-02-06 03:51:41 +0200

    fix all issues for score and add timer for each try

--- hangman.js
+++ hangman.js
@@ -12,7 +12,8 @@ var wrongGuess = [];
 var scoreElement = document.getElementById("score");
 var wordContent = document.getElementById("guessedWord");
 var checkGuess = document.getElementById("inputId");
-var scoreMinus = 0;
+let time=0;
+let timeSco=0;
 
 document.getElementById("firstg").addEventListener("click", function () {
   allGuess = [];
@@ -21,39 +22,37 @@ document.getElementById("firstg").addEventListener("click", function () {
   wordContent.innerHTML = "";
   staged = 0;
   score = 0;
-  scoreMinus = 0;
   word = choseWord();
   underSGuessed = "-".repeat(word.word.length);
   wordContent.innerHTML = underSGuessed;
   console.log(word.word);
   document.getElementById("hint").classList.add("d-none");
   document.getElementById("hint1").innerHTML = ""
-  word = choseWord();
-  console.log(word.word);
-
   start(score, scoreElement);
   underSGuessed = "-".repeat(word.word.length);
   wordContent.innerHTML = underSGuessed;
-  document.getElementById("firstShow").classList.toggle("d-none");
-  document.getElementById("game").classList.toggle("d-none");
+  var timer=setInterval(function () {
+    time++;
+    document.getElementById('time').innerHTML=`${time}s`
+  if (time===20) {
+    time=0;
+    timeSco+=1;
+    score = scoreColl(wrongGuess.length, correctGuess.length,timeSco);
+    scoreElement.innerHTML = `score <i class="fa-solid fa-star text-warning"></i>:${score}`;
+  }
+    
+  },1000)
 });
 
 document.getElementById("btnId").addEventListener("click", function () {
+  time=0;
   document.getElementById("hint").classList.remove("d-none");
-  document.getElementById("hint").addEventListener("click", function () {
-    document.getElementById("hint1").innerHTML = word.hint;
-    wrongGuess.push(1);
-    score += -10;
-    score = (correctGuess.length * 10) + scoreMinus;
-    scoreElement.innerHTML = `score <i class="fa-solid fa-star text-warning"></i>:${score}`;
-  });
+  showHint();
   guess = wordContent.innerHTML;
   if (!allGuess.includes(checkGuess.value)) {
     allGuess.push(checkGuess.value);
-
     if (word.word.includes(checkGuess.value)) {
       correctGuess = [];
-
       for (let i = 0; i < word.word.length; i++) {
         if (word.word.charAt(i) === checkGuess.value) {
           underSGuessed =
@@ -61,33 +60,27 @@ document.getElementById("btnId").addEventListener("click", function () {
             checkGuess.value +
             underSGuessed.substring(i + 1, underSGuessed.length);
           wordContent.innerHTML = underSGuessed;
-
+          //import
           if (!underSGuessed.includes("-")) {
-            document.getElementById("game").classList.toggle("d-none");
-            document.getElementById("tryagain").classList.toggle("d-none");
-
-            document.getElementById(
-              "gamSt"
-            ).innerHTML = `We have a wineer &#128526<br><br><p class="text-center">score <i class="fa-solid fa-star text-warning"></i>:${score}</p>`;
+            document.getElementById("game").classList.replace("d-flex", "d-none");
+            document.getElementById("tryagain").classList.replace("d-none","d-flex");
           }
         }
         if (underSGuessed.charAt(i) != "-") {
           correctGuess.push(underSGuessed.charAt(i));
         }
-
+       
         if (document.getElementById("msg").classList.contains("d-none")) {
           document.getElementById("msg").classList.toggle("d-none");
         } else {
           document.getElementById("msg").innerText = "Good Job";
         }
       }
-      
+
     } else {
       staged++;
-      scoreMinus += -10;
-      wrongGuess.push(checkGuess.value);
 
-      drawHangman(staged);
+      wrongGuess.push(checkGuess.value); drawHangman(staged);
       if (document.getElementById("msg").classList.contains("d-none")) {
         document.getElementById("msg").classList.toggle("d-none");
         document.getElementById("msg").innerText = "Guess Again";
@@ -95,29 +88,28 @@ document.getElementById("btnId").addEventListener("click", function () {
         document.getElementById("msg").innerText = "Guess Again";
       }
     }
+    score = scoreColl(wrongGuess.length, correctGuess.length,timeSco);
+    document.getElementById("gamSt"
+    ).innerHTML = `We have a wineer &#128526<br><br><p class="text-center">score
+     <i class="fa-solid fa-star text-warning"></i>:${score}</p>`;
     checkGuess.value = "";
     if (staged > 7) {
-      document.getElementById("game").classList.toggle("d-none");
-      document.getElementById("tryagain").classList.toggle("d-none");
-      score = 10 + (correctGuess.length - wrongGuess.length) * 10;
+      document.getElementById("game").classList.replace("d-flex", "d-none");
+      document.getElementById("tryagain").classList.replace("d-none","d-flex");
       document.getElementById(
         "gamSt"
-      ).innerHTML = `Try Again &#128549<br><br><p class="text-center">score <i class="fa-solid fa-star text-warning"></i>:${score}</p>`;
+      ).innerHTML = `Try Again &#128549<br><br><p class="text-center">score
+       <i class="fa-solid fa-star text-warning"></i>:${score}</p>`;
     }
   } else {
     checkGuess.value = "";
-    
-
     if (document.getElementById("msg").classList.contains("d-none")) {
       document.getElementById("msg").classList.toggle("d-none");
       document.getElementById("msg").innerText = "You Entered This Value";
     } else {
       document.getElementById("msg").innerText = "You Entered This Value";
     }
-    score = (correctGuess.length * 10) + scoreMinus;
-    scoreElement.innerHTML = `score <i class="fa-solid fa-star text-warning"></i>:${score}`;
-  }
-  score = (correctGuess.length * 10) + scoreMinus;
+}
   scoreElement.innerHTML = `score <i class="fa-solid fa-star text-warning"></i>:${score}`;
 });
 
@@ -128,14 +120,28 @@ document.getElementById("newgame").addEventListener("click", function () {
   wordContent.innerHTML = "";
   staged = 0;
   score = 0;
-  scoreMinus = 0;
   word = choseWord();
   underSGuessed = "-".repeat(word.word.length);
   wordContent.innerHTML = underSGuessed;
-  console.log(word.word);
   document.getElementById("hint").classList.add("d-none");
   document.getElementById("hint1").innerHTML = ""
-  // start(score, scoreElement);
-  document.getElementById("firstShow").classList.toggle("d-none");
-  document.getElementById("game").classList.toggle("d-none");
+  document.getElementById("firstShow").classList.replace("d-none","d-flex");
+  document.getElementById("tryagain").classList.replace("d-flex","d-none");
+  time=0;
+  clearInterval(timer)
+
 });
+
+function scoreColl(wrong, corr,t) {
+  let score = corr + (wrong * -1)+ (t * -1);
+  return score * 10;
+}
+function showHint() {
+  document.getElementById("hint").addEventListener("click", function () {
+    document.getElementById("hint1").innerHTML = word.hint;
+    wrongGuess.push(1);
+    score += -10;
+
+    scoreElement.innerHTML = `score <i class="fa-solid fa-star text-warning"></i>:${score}`;
+  });
+}
--- start.js
+++ start.js
@@ -1,24 +1,13 @@
 import choseWord from "./word.js";
 import drawHangman from "./draw.js";
 function start(score,scoreElement) {
-    document.getElementById("game").classList.toggle("d-none");
-    document.getElementById("tryagain").classList.toggle("d-none");
-    document.getElementById("msg").classList.toggle("d-none");
+    document.getElementById("firstShow").classList.replace("d-flex","d-none");
+    document.getElementById("game").classList.replace("d-none","d-flex");
     scoreElement.innerHTML = `score <i class="fa-solid fa-star text-warning"></i>:${score}`;
     drawHangman(0);
+    
   }
   
   export default start;
 
 
-  // function start() {
-//   document.getElementById("game").classList.toggle("d-none");
-//   document.getElementById("tryagain").classList.toggle("d-none");
-//   document.getElementById("msg").classList.toggle("d-none");
-//   scoreElement.innerHTML = `score <i class="fa-solid fa-star text-warning"></i>:${score}`;
-//   drawHangman(0);
-//   word = choseWord();
-//   underSGuessed = "-".repeat(word.length);
-//   wordContent.innerHTML = underSGuessed;
-//   console.log(word);
-// }
